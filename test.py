76721c0572a7f9c6b9b8250f70599ca3dd9d5dd6
MarioFilco <m_filipov@yahoo.com>
2019-12-23 09:11:39 +0200

    4

---
+++ test.py
@@ -0,0 +1,21 @@
+import bs4 as bs
+import urllib.request
+import re
+
+sauce = urllib.request.urlopen('https://www.scorespro.com/soccer/date/20-12-2019/').read()
+
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+a = soup.find('div', {'id': 'mainfeed'}).find_all('div', {'class': 'compgrp'})
+
+pattern = r"(title=\")(?P<country>\w+)[:][ ](?P<league>[\w ]*)\b"
+for i in a:
+    # print(i.get_text())
+    # m = i.get_text()
+    # m = m.split("\n")
+    # print(type(m))
+    # print(m)
+
+    # print("-------------------------------------------")
+    print(i.contents[1])
+    # print("-------------------------------------------")
