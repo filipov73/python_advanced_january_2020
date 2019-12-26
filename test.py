f1ee6c3bb887e3e9454ba0c7ddcd6a299e33a863
MarioFilco <m_filipov@yahoo.com>
2019-12-26 22:10:21 +0200

    5

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import bs4 as bs
 import urllib.request
 import re
 
-sauce = urllib.request.urlopen('https://www.scorespro.com/soccer/date/20-12-2019/').read()
+sauce = urllib.request.urlopen('https://www.scorespro.com/soccer/date/14-12-2019/').read()
 
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
@@ -10,9 +10,25 @@ a = soup.find('div', {'id': 'mainfeed'}).find_all('div', {'class': 'compgrp'})
 
 pattern = r"(title=\")(?P<country>\w+)[:][ ](?P<league>[\w ]*)\b"
 i = a[-1]
+print(i)
 print(i.get_text())
-a = i.contents[0]
-print(a.find('a', {'id': 'leaguetitle'}).get_text().split(": "))
+
+print(i.find('a', {'id': 'leaguetitle'}).get_text().split(": "))
+for i in i.find_all('table', {'id': 'blocks'}):
+
+    hour_minute = i.find('td', {"class": "kick"}).get_text()
+    status = i.find('td', {"class": "status"}).get_text()
+    home = i.find('td', {"class": "home"}).find('a').get_text()
+    away = i.find('td', {"class": "away"}).find('a').get_text()
+    score = i.find('td', {"class": "score"}).text
+    halftime = i.find('td', {"class": "halftime"}).text
+
+    print(hour_minute, end=" ")
+    print(status, end=" ")
+    print(home, end=" ")
+    print(away, end=" ")
+    print(score, end=" ")
+    print(halftime)
 
 # for i in a:
     # print(i.get_text())
@@ -24,3 +40,4 @@ print(a.find('a', {'id': 'leaguetitle'}).get_text().split(": "))
     # print("-------------------------------------------")
     # print(i.contents[1])
     # print("-------------------------------------------")
+
