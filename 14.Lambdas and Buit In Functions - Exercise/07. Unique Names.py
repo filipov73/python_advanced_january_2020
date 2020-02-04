2c6a50c8237f35b828d90db0dbbb539371cf1272
Martin Filipov <m_filipov@yahoo.com>
2020-02-04 21:57:01 +0200

    6h

--- 14.Lambdas and Buit In Functions - Exercise/07. Unique Names.py
+++ 14.Lambdas and Buit In Functions - Exercise/07. Unique Names.py
@@ -8,3 +8,8 @@ names = input().split()
 valid_names = filter(is_valid, names)
 result = sum([len(name) for name in valid_names])
 print(result)
+
+# names = input().split()
+# filtered = filter(lambda x: x.istitle(), names)
+# sum_lengths = sum(map(lambda x: len(x), filtered))
+# print(sum_lengths)
--- 14.Lambdas and Buit In Functions - Exercise/09. Odd or Even.py
+++ 14.Lambdas and Buit In Functions - Exercise/09. Odd or Even.py
@@ -9,3 +9,9 @@ if command == 'Odd':
 else:
     result = sum([x * length for x in numbers if x % 2 == 0])
 print(result)
+
+
+# def number_filter(command, *args):
+#     remainder = 0 if command == "Even" else 1
+#     return filter(lambda x: x % 2 == remainder, args)
+
