7f0c011b00854f6d5ce8f13397f45dba693f39e0
Martin Filipov <m_filipov@yahoo.com>
2020-02-16 20:32:14 +0200

    6h

---
+++ 18.Error Handling - Exercise/1.Numbers Dictionary.py
@@ -0,0 +1,33 @@
+# numbers_dictionary = {}
+#
+# line = input()
+#
+# while line != "Search":
+#     try:
+#         number_as_string = line
+#         number = int(input())
+#         numbers_dictionary[number_as_string] = number
+#     except ValueError:
+#         print("The variable number must be an integer")
+#
+#     line = input()
+#
+#
+# line = input()
+#
+# while line != "Remove":
+#     searched = line
+#     if searched in numbers_dictionary.keys():
+#         print(numbers_dictionary[searched])
+#     else:
+#         print("Number does not exist in dictionary")
+#     line = input()
+#
+# line = input()
+#
+# while line != "End":
+#     searched = line
+#     del numbers_dictionary[searched]
+#     line = input()
+#
+# print(numbers_dictionary)
